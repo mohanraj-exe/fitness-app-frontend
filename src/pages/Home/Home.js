b5fcdd0195914f62c6fa0c2aa6838baa3847e453
mohanraj-exe <smohan1029@gmail.com>
2022-10-14 18:16:29 +0530

    updating files

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,33 +1,127 @@
 import React from "react";
 import "./home.css";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styled from "styled-components";
 
-// imports
-import Calender from "../../components/Calender";
+const Button = styled.button`
+  height: 35px;
+  width: 80px;
+  color: white;
+  background-color: #006edc;
+  border-radius: 35px;
+  font-size: 15px;
+  cursor: pointer;
+`;
 
 const Home = () => {
+  const [date, setDate] = useState(new Date());
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    if (date[0] && date[1]) {
+      const response = await fetch(
+        `/api/user/workout/search?startDate=${date[0]}&endDate=${date[1]}`,
+        { method: "GET" }
+      );
+
+      const json = await response.json();
+      if (response.ok) {
+        setData(json);
+        console.log("Search Results:", json);
+      }
+    }
+  };
+
   return (
-    <div>
-      <div className="header">
+    <>
+      <header className="header">
         <h1>Workout Log App</h1>
+      </header>
+
+      <div className="nav-info">
+        <span>
+          Want to save your today's workout?&nbsp;
+          <Link to="/workoutForm"><button className="click-here">Click Here!</button></Link>
+        </span>
       </div>
 
-      <div className="workoutForm">
-        <p>Want to save your today's workout?&nbsp;<Link to="/workoutForm">Click Here!</Link></p>
+      <div className="search-info">
+        <span>
+          Please select the specific dates range and click <i style={{fontWeight: 'bold'}}>Search</i> button. 
+          Saved workouts will be displayed below.
+        </span>
       </div>
 
       <div className="row">
-        <div className="col-4">
-
+        <div className="col-8">
+          <table>
+            {data.length > 0 ? (
+              <thead>
+                <tr>
+                  <td>Muscle Group</td>
+                  <td>Title</td>
+                  <td>Load in lbs</td>
+                  <td>Sets</td>
+                  <td>Reps</td>
+                  <td>Comments</td>
+                  <td>Date and Time</td>
+                </tr>
+              </thead>
+            ) : (
+              ""
+            )}
+            <tbody>
+              {data.map((data) => (
+                <tr key={data._id}>
+                  <td>{data.muscleGroup}</td>
+                  <td>{data.title}</td>
+                  <td>{data.load}</td>
+                  <td>{data.sets}</td>
+                  <td>{data.reps}</td>
+                  <td>{data.comments}</td>
+                  <td>{data.createdAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <div className="col-4">
 
-        </div>
         <div className="col-4">
-          <Calender />
+          <div>
+            <Calendar
+              onChange={setDate}
+              value={date}
+              selectRange={true}
+              maxDate={new Date()}
+              minDate={new Date(2022, 6, 1)}
+            />
+          </div>
+          {date.length > 0 ? (
+            <p>
+              <span style={{ fontWeight: "bolder" }}>
+                Selected Date Ranges:
+              </span>
+              <br />
+              <span style={{ fontWeight: "bolder" }}>Start:</span>{" "}
+              {date[0].toDateString()}
+              &nbsp;|&nbsp;
+              <span style={{ fontWeight: "bolder" }}>End:</span>{" "}
+              {date[1].toDateString()}
+            </p>
+          ) : (
+            <p>
+              <span style={{ fontWeight: "bolder" }}>Date:</span>{" "}
+              {date.toDateString()}
+            </p>
+          )}
+
+          <Button onClick={fetchData}>Search</Button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/WorkoutForm/WorkoutForm.js
+++ src/pages/WorkoutForm/WorkoutForm.js
@@ -1,34 +1,182 @@
 import React from "react";
 import "./workoutForm.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import styled from "styled-components";
 
-// imports
-import WorkoutForm from "../../components/WorkoutForm";
+const Container = styled.div`
+  width: 455px;
+  height: 450px;
+  border: 1.5px solid lightgrey;
+  border-radius: 1.5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
 
-const Home = () => {
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+const Inputs = styled.div`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  height: 35px;
+  width: 80px;
+  color: white;
+  background-color: #006edc;
+  border-radius: 35px;
+  font-size: 15px;
+  cursor: pointer;
+`;
+
+const Error = styled.span`
+  background-color: red;
+  color: white;
+`;
+
+const WorkoutForm = () => {
+  const [muscleGroup, setMuscleGroup] = useState("");
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+  const [comments, setComments] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { muscleGroup, title, load, sets, reps, comments };
+
+    const response = await fetch("/api/user/workout", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+    }
+    if (response.ok) {
+      setMuscleGroup("");
+      setTitle("");
+      setLoad("");
+      setSets("");
+      setReps("");
+      setComments("");
+      setError("");
+      console.log("new workout added:", json);
+    }
+  };
+
   return (
     <div>
-      <div className="header">
+      <header className="header">
         <h1>Workout Log App</h1>
+      </header>
+
+      <div className="nav-info">
+        <span>
+          Want to get your saved workouts?&nbsp;<Link to="/"><button className="click-here">Click Here!</button></Link>
+        </span>
       </div>
 
-      <div className="workoutForm">
-        <p>Get saved workouts!!!&nbsp;<Link to="/">Click Here!</Link></p>
+      <div className="save-info">
+        <span>
+          Please fill your workout form and click <i style={{fontWeight: 'bold'}}>Save</i> button. 
+          Your workouts will be saved.
+        </span>
       </div>
 
       <div className="row">
-        <div className="col-4">
-          
-        </div>
-        <div className="col-3">
-          
-        </div>
+        <div className="col-4"></div>
+        <div className="col-3"></div>
         <div className="col-5">
-          <WorkoutForm />
+          <Container className="form-container">
+            <form onSubmit={handleSubmit}>
+              <H2>Workout Form</H2>
+
+              <Inputs>
+                <input
+                  type="text"
+                  onChange={(e) => setMuscleGroup(e.target.value)}
+                  name="muscleGroup"
+                  value={muscleGroup}
+                  placeholder="Muscle Group...Eg: Chest,Triceps,Biceps"
+                  style={{ margin: "10px", height: "35px" }}
+                />
+
+                <input
+                  type="text"
+                  onChange={(e) => setTitle(e.target.value)}
+                  name="title"
+                  value={title}
+                  placeholder="Workout Name...Eg: Flat Bench Press,Tricep Extensions"
+                  style={{ margin: "10px", height: "35px" }}
+                />
+
+                <input
+                  type="number"
+                  onChange={(e) => setLoad(e.target.value)}
+                  name="load"
+                  value={load}
+                  placeholder="Load...Eg: 315,275,125 lbs"
+                  style={{ margin: "10px", height: "35px" }}
+                />
+
+                <input
+                  type="number"
+                  onChange={(e) => setSets(e.target.value)}
+                  name="sets"
+                  value={sets}
+                  placeholder="Sets...Eg: 1,2,3 sets"
+                  style={{ margin: "10px", height: "35px" }}
+                />
+
+                <input
+                  type="number"
+                  onChange={(e) => setReps(e.target.value)}
+                  name="reps"
+                  value={reps}
+                  placeholder="Reps...Eg: 12,10,8,6 reps"
+                  style={{ margin: "10px", height: "35px" }}
+                />
+
+                <textarea
+                  onChange={(e) => setComments(e.target.value)}
+                  name="comments"
+                  value={comments}
+                  placeholder="Comments...Eg: Easy, Medium, Hard etc.."
+                  style={{ margin: "10px", height: "35px" }}
+                />
+              </Inputs>
+
+              <ButtonContainer>
+                <Button>Save</Button>
+              </ButtonContainer>
+
+              <Error>{error && <div className="error">{error}</div>}</Error>
+            </form>
+          </Container>
         </div>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default WorkoutForm;
